09a3bad9809046d77fe4800bae35bd5039724127
Majid Hussain <majid.hussain@Majids-MacBook-Pro.local>
2020-03-15 16:23:23 +0400

    Updates

---
+++ precache-manifest.3c810bcf61734c2bc9538a82c656e53d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2290c6c60dc333005a05032becd3e7c",
+    "url": "/corona-viz/index.html"
+  },
+  {
+    "revision": "56ff51e386f4585e351f",
+    "url": "/corona-viz/static/css/2.5b4252a3.chunk.css"
+  },
+  {
+    "revision": "5c1d92e591973dd1cfa2",
+    "url": "/corona-viz/static/css/main.2b5f6fe6.chunk.css"
+  },
+  {
+    "revision": "56ff51e386f4585e351f",
+    "url": "/corona-viz/static/js/2.f3518347.chunk.js"
+  },
+  {
+    "revision": "6dd85baa7da2a3c38282ba7158a3d4ae",
+    "url": "/corona-viz/static/js/2.f3518347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c1d92e591973dd1cfa2",
+    "url": "/corona-viz/static/js/main.a539da55.chunk.js"
+  },
+  {
+    "revision": "3829cb46574e98327a48",
+    "url": "/corona-viz/static/js/runtime-main.dcf4b358.js"
+  }
+]);
